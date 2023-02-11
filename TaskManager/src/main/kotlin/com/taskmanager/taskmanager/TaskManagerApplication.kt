162f8c0ac8040fcb4d55897ff013d614f783cf09
Paul Odhiambo <paulodhiambo962@gmail.com>
2023-02-11 20:28:32 +0300

    project initialization

---
+++ TaskManager/src/main/kotlin/com/taskmanager/taskmanager/TaskManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.taskmanager.taskmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskManagerApplication>(*args)
+}
